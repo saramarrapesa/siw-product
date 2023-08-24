26c45161f1bafece99a77a478a787ba432eb391d
saramarrapesa <115159885+saramarrapesa@users.noreply.github.com>
2023-08-24 20:23:08 +0200

    progetto product

---
+++ src/main/java/it/uniroma3/siw/siwproduct/SiwProductApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siwproduct/SiwProductApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
